e475c3587523ef1ba94bf45fb7f693ec674d4166
Lucas Sousa <Lucas996oliveira@live.com>
2020-01-07 15:07:38 +0000

    Adding config

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+    host:'postgres://ywwlmbwm:l49HII...@tuffi.db.elephantsql.com:5432/ywwlmbwm',
+    username:'ywwlmbwm',
+    password:'l49HIIHXXH8-ZcQVdcLkPmxpO0nyHPxX',
+    dialect:'postgres',
+    database:'ywwlmbwm',
+    oporatorAliases: false,
+    define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+
+}
