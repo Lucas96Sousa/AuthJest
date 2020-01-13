8764d7c0ab7a5ca6973e8e9d7b0357136c602af2
Lucas Sousa <37560590+Lucas96Sousa@users.noreply.github.com>
2020-01-13 01:30:51 -0300

    Auth TDD completo

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,15 +1,32 @@
 const request = require("supertest");
 
 const app = require("../../src/app");
-
-const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
+const factory = require("../factories");
 
 describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it("should authenticate with valid credentials", async () => {
-    const user = await User.create({
-      name: "Luca",
-      email: "luca@alpha.com",
-      password_hash: "1233312"
+    const user = await factory.create("User", {
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should not authenticate with invalid credentials", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
     });
 
     const response = await request(app)
@@ -19,6 +36,47 @@ describe("Authentication", () => {
         password: "123456"
       });
 
+    expect(response.status).toBe(401);
+  });
+
+  it("should return jwt token when authenticated", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+
+    expect(response.body).toHaveProperty("token");
+  });
+
+  it("should be able to access private routes when authenticated", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer ${user.generateToken()}`);
+
     expect(response.status).toBe(200);
+  });
+
+  it("should not be able to access private routes without jwt token", async () => {
+    const response = await request(app).get("/dashboard");
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should not be able to access private routes with invalid jwt token", async () => {
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", `Bearer 123123`);
+
+    expect(response.status).toBe(401);
   });
 });
--- src/config/database.js
+++ src/config/database.js
@@ -9,7 +9,7 @@ module.exports = {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT || "postgres",
   storage: "./__tests__/database.sqlite",
-  operatorsAliases: false,
+  operatorsAliases: 0,
   logging: false,
   define: {
     timestamps: true,
